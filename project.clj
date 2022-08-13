c3d78ef15ed03c7ce11daab90a3de633639569dc
Pedro Teles <pedroateles@protonmail.com>
2022-08-12 21:10:37 -0300

    Add github actions and lint config

--- project.clj
+++ project.clj
@@ -3,5 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :plugins [[com.github.clojure-lsp/lein-clojure-lsp "1.3.9"]]
   :dependencies [[org.clojure/clojure "1.10.3"]]
+  :aliases {"diagnostics"  ["clojure-lsp" "diagnostics"]
+            "format"       ["clojure-lsp" "format" "--dry"]
+            "format-fix"   ["clojure-lsp" "format"]
+            "clean-ns"     ["clojure-lsp" "clean-ns" "--dry"]
+            "clean-ns-fix" ["clojure-lsp" "clean-ns"]
+            "lint"         ["do" ["diagnostics"] ["format"] ["clean-ns"]]
+            "lint-fix"     ["do" ["format-fix"] ["clean-ns-fix"]]}
   :repl-options {:init-ns fakeflix-kafka.core})
