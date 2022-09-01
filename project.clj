b6b05f8b67f39803886e0e310b1ea71e6c31ab7f
Pedro Teles <pedroateles@protonmail.com>
2022-09-01 10:05:17 -0300

    fix: bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.pedroso/fakeflix-kafka "1.0.1-SNAPSHOT"
+(defproject org.clojars.pedroso/fakeflix-kafka "1.0.1"
   :description "Fakeflix Kafka library"
   :url "https://pedroteles.dev"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
